38ca6a9a9bf12978e0652767cdb297dad411bf36
Darya Malafeeva <malafeeva_dd@navis.ru>
2020-10-19 19:06:33 +0300

    построила реализации

--- dz1_MOPS_Malafeeva.py
+++ dz1_MOPS_Malafeeva.py
@@ -7,6 +7,7 @@ Created on Mon Oct 19 02:18:01 2020
 """
 import numpy as np
 import codecs
+import matplotlib.pyplot as plt
 
 """--------------------------Параметры моделирования------------------------"""
 f_s = 47.5*1e6
@@ -20,7 +21,7 @@ n       = np.random.normal(mu_n, sigma_n, M)
 
 #lam = np.array([A1], [A2], [w], [phi_0], [delta_phi])
 
-Dn = np.eye(5) * sigma_n
+Dn = np.eye(5) * sigma_n**2
 
 
 y1_list = []
@@ -46,7 +47,7 @@ with codecs.open('Input_Y0toT.txt', "r", encoding='utf-8', errors='ignore') as l
         
         
         # оценка максимального правдоподобия
-        lam_m = lam_op + u_d_n
+#        lam_m = lam_op + u_d_n
         
         
 
@@ -55,3 +56,24 @@ with codecs.open('Input_Y0toT.txt', "r", encoding='utf-8', errors='ignore') as l
 
 #   y1 = A1 * math.cos(w * k * T + phi_0) + n
 #    y2 = A1 * math.cos(w * k * T + phi_0) + n
+
+# строим реализации
+figure_1 = plt.figure(1)
+plt.plot(range(0, len(y1_list)), y1_list, color = 'deeppink', linewidth = 2)
+plt.plot(range(0, len(y2_list)), y2_list, color = 'orange', linewidth = 2)
+plt.plot(range(0, len(y3_list)), y3_list, color = 'gold', linewidth = 2)
+plt.plot(range(0, len(y4_list)), y4_list, color = 'limegreen', linewidth = 2)
+plt.xlabel('k')
+plt.ylabel('yn(k)')
+plt.legend(['y1','y2','y3', 'y4'])
+plt.title('Реализации на входе приёмника')
+plt.grid()
+plt.show()
+
+
+
+
+
+
+
+
