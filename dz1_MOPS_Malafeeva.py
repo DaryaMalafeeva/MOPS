a21b46609540b94357b276883fafd1f488a12f7c
Darya Malafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2020-10-28 16:46:20 +0300

    добавила все производные

--- dz1_MOPS_Malafeeva.py
+++ dz1_MOPS_Malafeeva.py
@@ -61,38 +61,116 @@ with codecs.open('Input_Y0toT.txt', "r", encoding='utf-8', errors='ignore') as l
         y4          = int(str_massive[3])
         y4_list.append(y4)
         # метод простой итерации
- #       while abs(delta_phi - delta_phi_old)> 1e-8:
+        while abs(delta_phi - delta_phi_old)> 1e-8:
             # сигналы
-            # S1 = A1 * math.cos(w * k * T + phi_0)
-            # S1_list.append(S1)
-            # S2 = A1 * math.sin(w * k * T + phi_0)
-            # S2_list.append(S2)
-            # S3 = A2 * math.cos(w * k * T + phi_0 + delta_phi)
-            # S3_list.append(S3)
-            # S4 = A2 * math.sin(w * k * T + phi_0 + delta_phi)
-            # S4_list.append(S4)
+            S1 = A1 * math.cos(w * k * T + phi_0)
+            S1_list.append(S1)
+            S2 = A1 * math.sin(w * k * T + phi_0)
+            S2_list.append(S2)
+            S3 = A2 * math.cos(w * k * T + phi_0 + delta_phi)
+            S3_list.append(S3)
+            S4 = A2 * math.sin(w * k * T + phi_0 + delta_phi)
+            S4_list.append(S4)
             
             # первые производные функции правдоподобия
-            # d_dA1 = 1/(sigma_n**2) * (sum(math.cos(w * k * T + phi_0) * y1_list +\
-            #         math.sin(w * k * T + phi_0) * y2_list) - M * A1)
+            d1_dA1 = 1/(sigma_n**2) *\
+                   (sum(math.cos(w * k * T + phi_0) * np.array(y1_list)+\
+                        math.sin(w * k * T + phi_0) * np.array(y2_list)) - M * A1)
+                
+            d1_dA2 = 1/(sigma_n**2) *\
+                   (sum(math.cos(w * k * T + phi_0 + delta_phi) * np.array(y3_list)+\
+                        math.sin(w * k * T + phi_0 + delta_phi) * np.array(y4_list)) - M * A2)
             
-        
-        #     #метод дискриминаторов
-        #     lam_array_old = lam_array
-        #     lam_array     = lam_array - L.transpose() * inv(H)
+            d1_dw  = 1/(sigma_n**2) *\
+                    (sum(-A1 * math.sin(w * k * T + phi_0) * k * T * np.array(y1_list)+\
+                          A1 * math.cos(w * k * T + phi_0) * k * T * np.array(y2_list)-\
+                          A2 * math.sin(w * k * T + phi_0 + delta_phi) * k * T * np.array(y3_list)+\
+                          A2 * math.cos(w * k * T + phi_0 + delta_phi) * k * T * np.array(y4_list)))
             
-        # # нижняя граница Рао-Крамера
-        # J = -inv(H)
-        # # ошибка оценивания delta_phi
-        # D_delta_phi = J[4,4]
+            d1_dphi_0 = 1/(sigma_n**2) *\
+                       (sum(-A1 * math.sin(w * k * T + phi_0) * np.array(y1_list)+\
+                             A1 * math.cos(w * k * T + phi_0) * np.array(y2_list)-\
+                             A2 * math.sin(w * k * T + phi_0 + delta_phi) * np.array(y3_list)+\
+                             A2 * math.cos(w * k * T + phi_0 + delta_phi) * np.array(y4_list)))
+                           
+            d1_ddelta_phi = 1/(sigma_n**2) *\
+                           (sum(-A2 * math.sin(w * k * T + phi_0 + delta_phi) * np.array(y3_list)+\
+                                 A2 * math.cos(w * k * T + phi_0 + delta_phi) * np.array(y4_list)))
+                               
+            L = np.array([d1_dA1, d1_dA2, d1_dw, d1_dphi_0, d1_ddelta_phi])
+            
+            # вторые и смешанные производные функции правдоподобия
+            d2_dA1    = (-M)/(sigma_n**2)
+            
+            d2_dA2    = (-M)/(sigma_n**2)
+            
+            d2_dA1dA2 = 0
+            
+            d2_dA1dw  = 1/(sigma_n**2) *\
+                       (sum(-math.sin(w * k * T + phi_0) * k * T * np.array(y1_list)+\
+                             math.cos(w * k * T + phi_0) * k * T * np.array(y2_list)))
+            
+            d2_dA1dphi_0 = 1/(sigma_n**2) *\
+                           (sum(-math.sin(w * k * T + phi_0) * np.array(y1_list)+\
+                                 math.cos(w * k * T + phi_0) * np.array(y2_list)))
+            
+            d2_dA1ddelta_phi = 0
+            
+            d2_dA2dw = 1/(sigma_n**2) *\
+                       (sum(-math.sin(w * k * T + phi_0 + delta_phi) * k * T * np.array(y3_list)+\
+                             math.cos(w * k * T + phi_0 + delta_phi) * k * T * np.array(y4_list)))
+            
+            d2_dA2ddelta_phi = 1/(sigma_n**2) *\
+                           (sum(-math.sin(w * k * T + phi_0 + delta_phi) * np.array(y3_list)+\
+                                 math.cos(w * k * T + phi_0 + delta_phi) * np.array(y4_list)))
+            
+            d2_dw = 1/(sigma_n**2) *\
+                       (sum(-A1 * math.sin(w * k * T + phi_0) * ((k * T)**2) * np.array(y1_list)+\
+                             A1 * math.cos(w * k * T + phi_0) * ((k * T)**2) * np.array(y2_list)-\
+                             A2 * math.sin(w * k * T + phi_0 + delta_phi) * ((k * T)**2) * np.array(y3_list)+\
+                             A2 * math.cos(w * k * T + phi_0 + delta_phi) * ((k * T)**2) * np.array(y4_list)))
+            
+            d2_dwdphi_0 = 1/(sigma_n**2) *\
+                       (sum(-A1 * math.cos(w * k * T + phi_0) * k * T * np.array(y1_list)-\
+                             A1 * math.sin(w * k * T + phi_0) * k * T * np.array(y2_list)-\
+                             A2 * math.cos(w * k * T + phi_0 + delta_phi) * k * T * np.array(y3_list)-\
+                             A2 * math.sin(w * k * T + phi_0 + delta_phi) * k * T * np.array(y4_list)))
+            
+            d2_dwddelta_phi = 1/(sigma_n**2) *\
+                           (sum(-A2 * math.cos(w * k * T + phi_0 + delta_phi) * k * T * np.array(y3_list)-\
+                                 A2 * math.sin(w * k * T + phi_0 + delta_phi) * k * T * np.array(y4_list)))
+                               
+            d2_dphi_0 = 1/(sigma_n**2) *\
+                       (sum(-A1 * math.cos(w * k * T + phi_0) * np.array(y1_list)-\
+                             A1 * math.sin(w * k * T + phi_0) * np.array(y2_list)-\
+                             A2 * math.cos(w * k * T + phi_0 + delta_phi) * np.array(y3_list)-\
+                             A2 * math.sin(w * k * T + phi_0 + delta_phi) * np.array(y4_list)))
+                           
+            d2_dphi_0ddelta_phi = 1/(sigma_n**2) *\
+                       (sum(-A2 * math.cos(w * k * T + phi_0 + delta_phi) * np.array(y3_list)-\
+                             A2 * math.sin(w * k * T + phi_0 + delta_phi) * np.array(y4_list)))
+                           
+            d2_ddelta_phi = 1/(sigma_n**2) *\
+                       (sum(-A2 * math.cos(w * k * T + phi_0 + delta_phi) * np.array(y3_list)-\
+                             A2 * math.sin(w * k * T + phi_0 + delta_phi) * np.array(y4_list)))
+            
+            H = np.array([],
+                         [],
+                         [],
+                         [],
+                         [])
+
+            # метод дискриминаторов
+            lam_array_old = lam_array
+            lam_array     = lam_array - L.transpose() * inv(H)
+            
+        # нижняя граница Рао-Крамера
+        J = -inv(H)
+        # ошибка оценивания delta_phi
+        D_delta_phi = J[4,4]
     
-        # шаг
-#        k +=1        
-
-
-
-
-
+        #шаг
+        k +=1        
 
 
 
