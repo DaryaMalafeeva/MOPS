37724acf0c88ae886e686cc77ad2d62259bf928c
Darya Malafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2020-10-29 12:29:38 +0300

    правки
    привела в адекватный вид, но точно есть ошибки в математике, оно не работает

--- dz1_MOPS_Malafeeva.py
+++ dz1_MOPS_Malafeeva.py
@@ -11,12 +11,13 @@ import matplotlib.pyplot as plt
 import math
 from numpy.linalg import inv
 
+"""---------------------------Обработка файла-------------------------------"""
 # инициализация списков
 y1_list = []
 y2_list = []
 y3_list = []
 y4_list = []
-"""---------------------------Обработка файла-------------------------------"""
+
 with codecs.open('Input_Y0toT.txt', "r", encoding='utf-8', errors='ignore') as log:
     for line in log:
         str_massive = line.split()
@@ -42,18 +43,10 @@ plt.grid()
 plt.show()
 
 """--------------------------Параметры моделирования------------------------"""
-f_s = 47.5*1e6
-T   = 1/f_s
-M   = 2048
-
-
-"""----------------------------Моделирование шума---------------------------"""
+f_s     = 47.5*1e6
+T       = 1/f_s
+M       = 2048
 sigma_n = 10
-mu_n    = 0
-n       = np.random.normal(mu_n, sigma_n, M)
-
-# матрица дисперсии шума наблюдений
-Dn = np.eye(5) * 1/(sigma_n**2)
 
 """---------------------Начальные параметры алгоритма-----------------------"""
 # начальные значения параметров вектора lam_array
@@ -63,11 +56,6 @@ f         = 0.25*1e6
 w         = 2 * math.pi * f
 phi_0     = 150 * math.pi /180
 delta_phi = 75 * math.pi / 180
-
-# начальное значение для метода простой итерации
-delta_phi_old = 0.1 * delta_phi
-
-# вектор оцениваемых параметров сигнала
 lam_array = np.array([A1, A2, w, phi_0, delta_phi])
 
 S1_list = []
@@ -76,9 +64,11 @@ S3_list = []
 S4_list = []
 
 """---------------------Алгоритм оценивания параметров----------------------"""
+# начальное значение для метода простой итерации
+delta_phi_old = 0.1 * delta_phi
+
 for k in range(1, M, 1):
     while abs(delta_phi - delta_phi_old) > 1e-8:
-        
         # первые производные функции правдоподобия
         d1_dA1 = 1/(sigma_n**2) *\
                 (sum(math.cos(w * k * T + phi_0) * np.array(y1_list)+\
@@ -165,10 +155,10 @@ for k in range(1, M, 1):
                     (sum(-A2 * math.cos(w * k * T + phi_0 + delta_phi) * np.array(y3_list)-\
                           A2 * math.sin(w * k * T + phi_0 + delta_phi) * np.array(y4_list)))
         
-        H = np.array([[d2_dA1, d2_dA1dA2, d2_dA1dw, d2_dA1dphi_0, d2_dA2ddelta_phi],
-                      [d2_dA1dA2, d2_dA2, d2_dA2dw, d2_A2dphi_0, d2_dA2ddelta_phi],
-                      [d2_dA1dw, d2_dA2dw, d2_dw, d2_dwdphi_0, d2_dwddelta_phi],
-                      [d2_dA1dphi_0, d2_A2dphi_0, d2_dwdphi_0, d2_dphi_0, d2_dphi_0ddelta_phi],
+        H = np.array([[d2_dA1,           d2_dA1dA2,        d2_dA1dw,        d2_dA1dphi_0,        d2_dA2ddelta_phi],
+                      [d2_dA1dA2,        d2_dA2,           d2_dA2dw,        d2_A2dphi_0,         d2_dA2ddelta_phi],
+                      [d2_dA1dw,         d2_dA2dw,         d2_dw,           d2_dwdphi_0,         d2_dwddelta_phi],
+                      [d2_dA1dphi_0,     d2_A2dphi_0,      d2_dwdphi_0,     d2_dphi_0,           d2_dphi_0ddelta_phi],
                       [d2_dA2ddelta_phi, d2_dA2ddelta_phi, d2_dwddelta_phi, d2_dphi_0ddelta_phi, d2_ddelta_phi]])
         
         # метод дискриминаторов
@@ -176,18 +166,19 @@ for k in range(1, M, 1):
         lam_array     = lam_array - np.dot(L,H)
         
         # обновляем параметры
-        A1        = lam_array[0]
-        A2        = lam_array[1]
-        w         = lam_array[2]
-        pho_0     = lam_array[3]
-        delta_phi = lam_array[4]
+        A1            = lam_array[0]
+        A2            = lam_array[1]
+        w             = lam_array[2]
+        pho_0         = lam_array[3]
+        delta_phi_old = delta_phi
+        delta_phi     = lam_array[4]
         
-    # нижняя граница Рао-Крамера
-    J = -inv(H)
+    # выход из цикла while
     # ошибка оценивания delta_phi
+    J = -inv(H)
     D_delta_phi = J[4,4]
     
-    # сигналы
+    # сигналы с оценками параметров
     S1 = A1 * math.cos(w * k * T + phi_0)
     S1_list.append(S1)
     S2 = A1 * math.sin(w * k * T + phi_0)
@@ -197,7 +188,17 @@ for k in range(1, M, 1):
     S4 = A2 * math.sin(w * k * T + phi_0 + delta_phi)
     S4_list.append(S4)
     
-     
+figure_2 = plt.figure(2)
+plt.plot(range(0, len(S1_list)), S1_list, color = 'deeppink', linewidth = 2)
+plt.plot(range(0, len(S2_list)), S2_list, color = 'orange', linewidth = 2)
+plt.plot(range(0, len(S3_list)), S3_list, color = 'gold', linewidth = 2)
+plt.plot(range(0, len(S4_list)), S4_list, color = 'limegreen', linewidth = 2)
+plt.xlabel('k')
+plt.ylabel('Sn(k)')
+plt.legend(['S1','S2','S3', 'S4'])
+plt.title('Сигналы с оценками параметров ')
+plt.grid()
+plt.show()   
 
 
 
