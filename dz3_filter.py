0f7b7e6d524e4a5337b4f4823f042b0096f3ca58
Darya Malafeeva <53153663+DaryaMalafeeva@users.noreply.github.com>
2020-12-14 17:03:18 +0300

    запускается
    но работает неправильно

--- dz3_filter.py
+++ dz3_filter.py
@@ -10,34 +10,31 @@ from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
 """-----------------------------Параметры моделирования---------------------"""
-T      = 10 * 1e-3 
-T_d    = 0.2 * 1e-6
-M      = 100000
-c      = 3* 1e8
-w0     = 2 * math.pi * 1602e6
-w_p    = 2 * math.pi * 2 * 1e6
-alpha  = 1
-q_c_n0 = 10 ** (0.1 * 30)
-t_list = []
-N      = T/T_d
+T        = 10 * 1e-3 
+T_d      = 0.2 * 1e-6
+mod_time = 2
+c        = 3* 1e8
+w0       = 2 * math.pi * 1602e6
+w_p      = 2 * math.pi * 2 * 1e6
+alpha    = 1
+q_c_n0   = 10 ** (0.1 * 30)
+t_list   = []
+N        = int(T/T_d)
 
 """-----------------------Моделируем шум------------------------------------"""
 # формирующий шум пси
 sigma_psi = 0.5
-psi_list  = np.random.normal(loc = 0.0, scale = sigma_psi * 1, size = M)
-psi_i     = np.array([[psi_list[0]]])
-
+psi_list  = np.random.normal(loc = 0.0, scale = sigma_psi * 1, size = N)
 
 # формирующий шум кси
 sigma_a   = 10
 S_ksi     = 2 * (sigma_a**2) * alpha * ((w0/c)**2)
 sigma_ksi = math.sqrt((S_ksi)/(2*T))
-ksi_list  = np.random.normal(loc = 0.0, scale = sigma_ksi * 1, size = M)
-ksi_i     = np.array([[ksi_list[0]]])
+ksi_list  = np.random.normal(loc = 0.0, scale = sigma_ksi * 1, size = N)
 
 # шум наблюдения
 sigma_n   = 35.4
-n_list    = np.random.normal(loc = 0.0, scale = sigma_n * 1, size = M)
+n_list    = np.random.normal(loc = 0.0, scale = sigma_n * 1, size = N)
 
 """---------------------------Инициализация--------------------------------"""
 # начальные значения истинных параметров
@@ -45,23 +42,13 @@ a_true          = 1
 phi_true        = math.pi/12
 Omega_true      = 100
 v_true          = 100
-a_true_list     = []
-phi_true_list   = []
-Omega_true_list = []
-v_true_list     = []
 
 # начальные значения скорректированных оценок
 a_corr                = 0.5
 phi_corr              = 0
 Omega_corr            = 0
 v_corr                = 0
-a_corr_list           = []
-phi_corr_list         = []
-Omega_corr_list       = [] 
-v_corr_list           = [] 
-sigma_Omega_corr_list = []
-Eps_Omega_list        = []
-D11_list              = []
+
 
 # матрицы фильтра
 X_true   = np.array([[a_true],\
@@ -69,97 +56,114 @@ X_true   = np.array([[a_true],\
                      [Omega_true],\
                      [v_true]])
 
-# X_corr   = np.array([[a_corr],\
-#                      [phi_corr],\
-#                      [Omega_corr],\
-#                      [v_corr]])                                 # 2x1
+X_corr   = np.array([[a_corr],\
+                      [phi_corr],\
+                      [Omega_corr],\
+                      [v_corr]])                                     # 4x1
     
-# I        = np.eye(4)                                                # 2x2
+I        = np.eye(4)                                                # 4x4
 
-# H        = np.array([[1, 0]])                                       # 1x2
+C        = np.array([[1, 0, 0, 0],\
+                     [0, 1, 0, 0]])                                       # 1x2
 
-# D_x_corr = np.array([[(0.3)**2, 0, 0, 0],\
-#                      [0, (math.pi)**2, 0, 0],\
-#                      [0, 0, 34**2, 0],\
-#                      [0, 0, 0, 340**2]])                              # 2x2
+D_x_corr = np.array([[(0.3)**2, 0, 0, 0],\
+                      [0, (math.pi)**2, 0, 0],\
+                      [0, 0, 34**2, 0],\
+                      [0, 0, 0, 340**2]])                             # 4x4
    
 G        = np.array([[T,         0],\
                      [0,         0],\
                      [0,         0],\
-                     [0, alpha * T]])                                  # 2x1
+                     [0, alpha * T]])                                 # 4x1
 
-# D_ksi    = np.array([[sigma_ksi**2]])                               # 1x1
+D_ksi    = np.array([[sigma_psi**2,            0],\
+                     [           0, sigma_ksi**2]])                   # 2x2
 
-# D_n      = np.array([[sigma_n**2]])  
+    
+psi_ksi  = np.array([[psi_list[0]],\
+                     [ksi_list[0]]])       
+    
+    
+D_n      = np.array([[sigma_n**2]])                                    #1x1
 
 F        = np.array([[1, 0, 0,               0],\
                      [0, 1, T,               0],\
                      [0, 0, 1,               T],\
-                     [0, 0, 0, (1 - alpha * T)]])                    # 4X4
+                     [0, 0, 0, (1 - alpha * T)]])                    # 4x4
 
-    
-psi_ksi  = np.array([[psi_i],\
-                     [ksi_i]])    
-    
 
-# внешний цикл быстрый по Td
-# внутренний должен быть медленным по T
-# в моменты когда Td=T производим коррекцию    
-i = 0
-k = 0
-while t_i < M:
-    i+=1
-    t_i = i*T_d
+W11      = (N)/(2 * sigma_n**2)
+W12      = 0
+W21      = 0
+# W22 пересчитывается после экстраполяции
+W22      = 0
+W        = np.array([[W11, W12],\
+                     [W21, W22]])
+    
+    
+k   = 0
+t_k = 0
+while t_k < mod_time:
+    k+=1
+    t_k +=T
+    
     """----------------------Входное воздействие----------------------------"""
     X_true  = F.dot(X_true) + G.dot(psi_ksi)
-    a_true_list.append(X_true[0])
-    phi_true_list.append(X_true[1])
-    Omega_true_list.append(X_true[2])
-    v_true_list.append(X_true[3])
     
-    # ksi_k        = np.array([[ksi_list[k]]])
+    psi_ksi  = np.array([[psi_list[k]],\
+                         [ksi_list[k]]]) 
+        
+    # амплитуда
+    if t_k < 1:
+        X_true[0][0] = 0.5
+    elif t_k >= 1:
+        X_true[0][0] = 1
+    
+    """----------------------------Экстраполяция----------------------------"""
+    X_extr       = F.dot(X_corr)                                   # 4x1
+    
+    D_x_extr_pt1 = (F.dot(D_x_corr)).dot(F.transpose())            # 4x4
 
-    # y            = H.dot(X_true) + n_list[k]
+    D_x_extr_pt2 = (G.dot(D_ksi)).dot(G.transpose())               # 4x4
+
+    D_x_extr     = D_x_extr_pt1 + D_x_extr_pt2                     # 4x4
+
+    W22          = (N *((X_extr[0][0])**2))/(2 * sigma_n**2)
     
-    while t_k < M/N:
-        k+=1
-        t_k = k*T
-        if t_i == t_k:
-        
-    #     """------------------------------Фильтрация-----------------------------"""
-        
+    """--------------------------Коррелятор---------------------------------"""
     
-    #     """----------------------------Экстраполяция----------------------------"""
-    #     X_extr       = F.dot(X_corr)                                   # 2x1
-        
-    #     D_x_extr_pt1 = (F.dot(D_x_corr)).dot(F.transpose())            # 2x2
+    # фаза
+    phi_p = np.array(range(0, N, 1)) * T_d * w_p
     
-    #     D_x_extr_pt2 = (G.dot(D_ksi)).dot(G.transpose())               # 2x2
+    y     = X_true[0][0] * np.cos(phi_p + X_true[1][0]) + np.random.random(N) * sigma_n
     
-    #     D_x_extr     = D_x_extr_pt1 + D_x_extr_pt2                     # 2x2
-        
-    #     """------------------------------Коррекция------------------------------"""
-    #     K            = (D_x_extr.dot(H.transpose())).dot(inv(((H.dot(D_x_extr)).dot(H.transpose())) + D_n))  # 2x1
-        
-    #     D_x_corr     = (I - K.dot(H)).dot(D_x_extr)                    # 2x2        
-        
-    #     X_corr       = X_extr + K.dot(y - H.dot(X_extr))               # 2x1
-        
-    #     # мгновенная ошибка фильтрации 
-    #     Eps_Omega    = X_corr[0] - X_true[0]
-    #     Eps_Omega_list.append(Eps_Omega)
-        
-    #     Omega_corr   = X_corr[0]
-    #     Omega_corr_list.append(Omega_corr)
-        
-    #     v_corr       = X_corr[1]
-    #     v_corr_list.append(v_corr)    
+    S_sin = np.sin(phi_p + X_corr[1][0])
     
-    #     sigma_Omega_corr = math.sqrt(D_x_corr[0,0])
-    #     sigma_Omega_corr_list.append(sigma_Omega_corr)
-        
-    #     D11 = D_x_corr[0,0]
-    #     D11_list.append(D11)
+    S_cos = np.cos(phi_p + X_corr[1][0])
+    
+    I     = np.sum(np.multiply(y, S_cos.transpose()))
+    
+    Q     = np.sum(np.multiply(y, S_sin.transpose()))
+    
+    U_1   = I * (1/D_n) - (X_extr[0][0] * N)/(2 * D_n)
+    
+    U_2   = Q * (X_extr[0][0])/(D_n)
+    
+    U     = np.array([U_1[0],\
+                      U_2[0]])
+    """------------------------------Коррекция------------------------------"""
+    
+    D_x_corr     = inv(inv(D_x_extr) + ((C.transpose().dot(W)).dot(C)))  # 4x4        
+    
+    X_corr       = X_extr + ((D_x_corr.dot(C.transpose())).dot(U))     # 4x1
+    
+    print('--------------')
+    print('Шаг №' + str(k))
+    print(X_corr)
+    print('             ')
+    
+    
+
     
 # plt.figure(1)
 # plt.plot(t_list[0::], Omega_true_list[0::], '.', color = 'mediumblue', linewidth = 1)
